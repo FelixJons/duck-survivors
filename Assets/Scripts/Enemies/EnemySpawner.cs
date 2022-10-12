b270aa9c14d5803853a0886fb5775369a9436631
Felix J <felix.jonsson1993@gmail.com>
2022-10-12 17:23:13 +0200

    Fix correct available spawn points for enemies

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -12,46 +12,60 @@ namespace Enemies
         [SerializeField] private int pixelsPerUnit = 16;
         [SerializeField] private Transform player;
 
-        // Get all eligible grid cells that are not in the camera view
-        private TileBase[] allTiles;
         private Camera mainCamera;
 
         private void Start()
         {
-            BoundsInt tileBounds = tilemap.cellBounds;
-            allTiles = tilemap.GetTilesBlock(tileBounds);
             mainCamera = Camera.main;
-        
+            
             Spawn();
         }
 
-        private List<Vector3> GetCellsOutsideCameraView()
+        private bool CellHasCollider(Vector3 cellWorldPos)
         {
-            List<Vector3> spawnPositions = new List<Vector3>();
-
-            foreach (Vector3Int cellPosition in tilemap.cellBounds.allPositionsWithin)
-            {
-                Vector3 worldPosition = tilemap.CellToWorld(cellPosition);
-                Vector3 screenPosition = mainCamera.WorldToScreenPoint(worldPosition);
-                if (screenPosition.x + pixelsPerUnit >= 0 && screenPosition.x - pixelsPerUnit <= Screen.width &&
-                    screenPosition.y + pixelsPerUnit >= 0 && screenPosition.y - pixelsPerUnit <= Screen.height)
-                {
-                    continue;
-                }
-
-                spawnPositions.Add(worldPosition);
-            }
-
-            return spawnPositions;
+            var c = Physics2D.OverlapBox((Vector2) cellWorldPos + new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), 0);
+            return c != null;
         }
-    
+
+        private bool CellIsInsideCamera(Vector3 cellWordPos)
+        {
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(cellWordPos);
+            return screenPosition.x + pixelsPerUnit >= 0 && screenPosition.x - pixelsPerUnit <= Screen.width &&
+                   screenPosition.y + pixelsPerUnit >= 0 && screenPosition.y - pixelsPerUnit <= Screen.height;
+        }
+
+
         private void Spawn()
         {
-            foreach(Vector3 position in GetCellsOutsideCameraView())
+            List<Vector3> availableSpawnPositions = GetAvailableSpawnPositions();
+
+            foreach (Vector3 worldPosition in availableSpawnPositions)
             {
-                var e = Instantiate(enemy, position, quaternion.identity);
+                var e = Instantiate(enemy, worldPosition, quaternion.identity);
                 e.GetComponent<Enemy>().player = player;
             }
+        }
+
+        private List<Vector3> GetAvailableSpawnPositions()
+        {
+            List<Vector3> availableSpawnPositions = new List<Vector3>();
+
+            foreach (Vector3Int pos in tilemap.cellBounds.allPositionsWithin)
+            {
+                Vector3Int localPosition = new Vector3Int(pos.x, pos.y, pos.z);
+                Vector3 worldPosition = tilemap.CellToWorld(localPosition);
+                if (tilemap.HasTile(localPosition))
+                {
+                    if (CellHasCollider(worldPosition) || CellIsInsideCamera(worldPosition))
+                    {
+                        continue;
+                    }
+
+                    availableSpawnPositions.Add(worldPosition);
+                }
+            }
+
+            return availableSpawnPositions;
         }
     }
 }
